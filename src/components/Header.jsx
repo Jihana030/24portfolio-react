bb022a23536c75ea44a34a2c25f21bebee4037fa
jiwon <wldnjs7618@naver.com>
2024-03-26 13:47:09 +0900

    header set

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+
+function Header() {
+  return (
+    <header>
+      <div class="logo_wrap">
+        <p>
+          <a href="../index.html">logo</a>
+        </p>
+      </div>
+      <nav class="nav_wrap">
+        <ul>
+          <li>
+            <a href="">이력서/자소서</a>
+          </li>
+          <li>
+            <a href="https://github.com/Jihana030">깃허브</a>
+          </li>
+          <li>
+            <a href="https://www.figma.com/file/T884LiS5OO7vNoSA3sVZJp/portfolio?type=design&node-id=301%3A50&mode=design&t=nAYo4Oam72R1WMsV-1">
+              피그마
+            </a>
+          </li>
+          <li>
+            <a href="/html/varies.html">카테고리</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
