9911ddca28feeee26b101294332f3c0738978a4e
jiwon <wldnjs7618@naver.com>
2024-04-04 17:17:42 +0900

    route set

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Layout() {
+  const github = "https://github.com/Jihana030";
+  const figma =
+    "https://www.figma.com/file/T884LiS5OO7vNoSA3sVZJp/portfolio?type=design&node-id=301%3A50&mode=design&t=nAYo4Oam72R1WMsV-1";
+
   return (
     <header>
       <div className="logo_wrap">
         <p>
-          <a href="../index.html">logo</a>
+          <Link to="/">logo</Link>
         </p>
       </div>
       <nav className="nav_wrap">
         <ul>
           <li>
-            <a href="">이력서/자소서</a>
+            <Link>이력서/자소서</Link>
           </li>
           <li>
-            <a href="https://github.com/Jihana030">깃허브</a>
+            <a href={github}>깃허브</a>
           </li>
           <li>
-            <a href="https://www.figma.com/file/T884LiS5OO7vNoSA3sVZJp/portfolio?type=design&node-id=301%3A50&mode=design&t=nAYo4Oam72R1WMsV-1">
-              피그마
-            </a>
+            <a href={figma}>피그마</a>
           </li>
           <li>
-            <a href="/html/varies.html">카테고리</a>
+            <Link to="/varies">카테고리</Link>
           </li>
         </ul>
       </nav>
