a2945dcd881bbe4e86fe551f1f9b280d8e863c9b
jiwon <wldnjs7618@naver.com>
2024-04-11 17:32:02 +0900

    cardlist try false

--- src/Main.js
+++ src/Main.js
@@ -2,14 +2,17 @@ import MainDetail from './components/MainDetail';
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Varies from "./components/Varies";
+import React from 'react';
 
 function Main() {
+  const CardList = React.lazy(() => import("./components/CardList"));
   return (
     <BrowserRouter>
       <App>
         <Routes>
           <Route path="/" element={<MainDetail />} />
-          <Route path="/:varies" element={<Varies />} />
+          <Route path="/varies" element={<Varies />} />
+          <Route path="/varies/:id" element={<CardList />} />
           <Route path="/varies/luckydraw" element={<MainDetail />} />
           <Route path="/varies/guestbook" element={<MainDetail />} />
         </Routes>
