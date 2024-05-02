b9a3f11b1bf9346b0d21df0bf477eea448b46e6b
jiwon <wldnjs7618@naver.com>
2024-05-02 15:45:30 +0900

    harrypotter com make

---
+++ src/components/Harrypoter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Title from "./Title";
+import harryMain from '../assets/img/profile_replace.jpg'
+
+function Harryporter() {
+  return (
+    <div id="harry_wrap">
+      <Title title={"학원 마지막 프로젝트 : 호그와트 홈페이지"} />
+      <div className="harry_detail">
+        <div className="harry_img">
+          <img src={harryMain} alt="" />
+        </div>
+        <div className="detail_wrap">
+          <div className="title">
+            <h3>페이지 제목</h3>
+            <p className="arrow_wrap">
+              보러가기
+              <span class="material-symbols-outlined">arrow_forward</span>
+            </p>
+          </div>
+          <div className="detail">내용</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Harryporter;
