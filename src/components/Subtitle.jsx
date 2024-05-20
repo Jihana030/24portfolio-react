c387bb57290baec04590744d383ad4a472b23c3e
jiwon <wldnjs7618@naver.com>
2024-05-20 17:15:16 +0900

    subtitle modi

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -7,7 +7,7 @@ function Subtitle (){
     return(
         <div className="subtitle_wrap">
         <p>클릭하시면 샘플음원이 재생됩니다.</p>
-        <p>아직 녹음을 못했답니다.</p>
+        <p>안녕하세요. 프론트엔드 황지원입니다. 제 사이트에 방문해주셔서 감사합니다.</p>
       </div>
     )
 }
