5b74b27bd987ddd53dfdb240d6f4e0464f19bad7
jiwon <wldnjs7618@naver.com>
2024-05-20 11:48:34 +0900

    weather week,day add

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import TodayWeather from "./TodayWeather";
 import { useLightMode } from "./ModeContext";
 import WeekWeather from "./WeekWeather";
+import WeatherDaytime from "./WeatherDaytime";
 
 function Weather() {
   const { isLightMode } = useLightMode();
   return (
     <div id="weather" className={isLightMode ? "light" : "dark"}>
       <TodayWeather />
-      <WeekWeather/>
+      <div className="longWeather">
+        <WeatherDaytime />
+        <WeekWeather />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Weather;
