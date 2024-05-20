9e8ea1029a85c9b59f28668ae80dfa8ac9893abb
Jihana <wldnjs7618@naver.com>
2024-05-21 01:24:01 +0900

    nav mediaquery

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ThemeMode from "./ThemeMode";
 import { useLightMode } from "./ModeContext";
+import { useMediaQuery } from "react-responsive";
 
 export default function Layout() {
   const github = "https://github.com/Jihana030";
@@ -9,6 +10,13 @@ export default function Layout() {
     "https://www.figma.com/file/T884LiS5OO7vNoSA3sVZJp/portfolio?type=design&node-id=301%3A50&mode=design&t=nAYo4Oam72R1WMsV-1";
 
   const { isLightMode } = useLightMode();
+  const mobile = useMediaQuery({
+    query: "(max-width:700px)",
+  })
+  const [menu, setMenu]=useState(false);
+  const handleMenu=()=>{
+    setMenu(!menu);
+  }
   return (
     <header className={isLightMode?'light':'dark'}>
       <div className="logo_wrap">
@@ -18,9 +26,10 @@ export default function Layout() {
         <ThemeMode />
       </div>
       <nav className="nav_wrap">
-        <ul>
+        {mobile&&<span className="hamburger material-symbols-rounded" onClick={handleMenu}>menu</span>}
+        {mobile||<ul className={`row`}>
           <li>
-            <Link to="/curriculum">프로필</Link>
+            <Link to="/curriculum" >프로필</Link>
           </li>
           <li>
             <a href={github}>깃허브</a>
@@ -31,7 +40,21 @@ export default function Layout() {
           <li>
             <Link to="/varies">카테고리</Link>
           </li>
-        </ul>
+        </ul>}
+        {mobile&&<ul className={`row ${menu && ' dis-none'}`}>
+          <li>
+            <Link to="/curriculum" >프로필</Link>
+          </li>
+          <li>
+            <a href={github}>깃허브</a>
+          </li>
+          <li>
+            <a href={figma}>피그마</a>
+          </li>
+          <li>
+            <Link to="/varies">카테고리</Link>
+          </li>
+        </ul>}
       </nav>
     </header>
   );
