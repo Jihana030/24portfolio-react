a43163186624a4f0135a6da4f86cdfcc7e790baf
jiwon <wldnjs7618@naver.com>
2024-05-22 16:09:20 +0900

    navlink, route add

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,15 @@
-import MainDetail from './components/MainDetail';
+import MainDetail from "./components/MainDetail";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Varies from "./components/Varies";
-import React from 'react';
-import GuestBook from './components/GuestBook';
-import GuestBookWrite from './components/GuestBookWrite';
-import Luckydraw from './components/Luckydraw';
-import Making from './components/Making';
-import Harrypotter from './components/Harrypotter';
-import Curriculum from './components/Curriculum';
-import Weather from './components/Weather';
+import React from "react";
+import GuestBook from "./components/GuestBook";
+import GuestBookWrite from "./components/GuestBookWrite";
+import Luckydraw from "./components/Luckydraw";
+import Making from "./components/Making";
+import Harrypotter from "./components/Harrypotter";
+import Curriculum from "./components/Curriculum";
+import Weather from "./components/Weather";
 
 function Main() {
   const CardList = React.lazy(() => import("./components/CardList"));
@@ -18,15 +18,17 @@ function Main() {
       <App>
         <Routes>
           <Route path="/" element={<MainDetail />} />
-          <Route path="/varies" element={<Varies />} />
-          <Route path="/varies/:id" element={<CardList />} />
-          <Route path="/varies/luckydraw" element={<Luckydraw />} />
-          <Route path="/varies/making" element={<Making />} />
-          <Route path="/varies/harrypotter" element={<Harrypotter />} />
-          <Route path="/varies/guestBook" element={<GuestBook />} />
-          <Route path="/varies/guestBook/guestBookWrite" element={<GuestBookWrite />} />
-          <Route path="/curriculum" element={<Curriculum />} />
-          <Route path="/varies/weather" element={<Weather />} />
+          <Route path="varies">
+            <Route index element={<Varies />} />
+            <Route path=":id" element={<CardList />} />
+            <Route path="luckydraw" element={<Luckydraw />} />
+            <Route path="making" element={<Making />} />
+            <Route path="harrypotter" element={<Harrypotter />} />
+            <Route path="guestBook" element={<GuestBook />} />
+            <Route path="guestBook/guestBookWrite" element={<GuestBookWrite />} />
+            <Route path="weather" element={<Weather />} />
+          </Route>
+          <Route path="curriculum" element={<Curriculum />} />
         </Routes>
       </App>
     </BrowserRouter>
